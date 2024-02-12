1c3fd99b22a376133a4d13b378cbcb660930e12f
shumancheng <Shumancheng0119@gmail.com>
2024-02-12 19:58:39 +0100

    Update index.html

---
+++ JavaScriptSmallProject/Notes App/script.js
@@ -0,0 +1,12 @@
+const noteContainer = document.querySelector(".Note-container");
+const createNote = document.querySelector("createNote");
+let notes = document.querySelectorAll(".input-box");
+
+function createNote(){
+    let inputBox = document.createElement("p");
+    noteBox.appendChild(inputBox);
+}
+
+createNote.addEventListener("click", ()=>{
+    
+})
