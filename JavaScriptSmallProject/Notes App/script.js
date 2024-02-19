af8d0a43a2fe8b53142f868fa374e573abcd620f
shumancheng <Shumancheng0119@gmail.com>
2024-02-19 22:36:54 +0100

    Update script.js

--- JavaScriptSmallProject/Notes App/script.js
+++ JavaScriptSmallProject/Notes App/script.js
@@ -1,6 +1,17 @@
 const notesContainer = document.querySelector(".Note-container");
-const createNotes = document.querySelector("#createNotes");
+const createNotes = document.querySelector("#createNotes"); //The # symbol is used to select an element by its ID
 let notes = document.querySelectorAll(".input-box");
+
+function showNotes(){
+    notesContainer.innerHTML = localStorage.getItem("Notes");
+}
+
+showNotes()
+
+
+function updateStorage(){
+    localStorage.setItem("notes", notesContainer.innerHTML);
+}
 
 createNotes.addEventListener("click", ()=>{
     let inputBox = document.createElement("p");
@@ -9,4 +20,19 @@ createNotes.addEventListener("click", ()=>{
     inputBox.setAttribute("contenteditable", "true");
     img.src = "notes-app-img/images/delete.png";
     notesContainer.appendChild(inputBox).appendChild(img);
+})
+
+notesContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "IMG"){
+        e.target.parentElement.remove();
+        updateStorage()
+    }
+    else if(e.target.tagName === "P"){
+        notes = document.querySelectorAll(".input-box");
+        notes.forEach(nt => {
+            nt.onkeyup = function(){
+                updateStorage();
+            }
+        })
+    }
 })
